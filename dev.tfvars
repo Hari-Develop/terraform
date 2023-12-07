fa777d4cfa808436ec090f6feb4ffbaf395379f7
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-12-07 20:58:57 +0530

    commit

---
+++ dev.tfvars
@@ -0,0 +1,8 @@
+ami = "ami-03265a0778a880afb"
+instance_type = "t2.micro"
+component = {
+  tags = {
+    name = "test"
+  }
+
+}
--- main.tf
+++ main.tf
@@ -1,23 +1,9 @@
 resource "aws_instance" "test" {
   ami = var.ami
   instance_type = var.instance_type
-  tags = var.name["tags"]
+  tags = var.component["tags"]
 }
 
-variable "ami" {
-  default = "ami-03265a0778a880afb"
-}
 
-variable "instance_type" {
-  default = "t2.micro"
-}
 
-variable "name" {
-  default = {
-    tags = {
-      name = "test"
-    }
-
-  }
-}
 
