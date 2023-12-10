6a21130e03da480f0da34cb5c8bed5579269a6ab
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-12-10 13:12:22 +0530

    commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,5 +7,5 @@ resource "aws_subnet" "public" {
   count = length(var.public_subnet)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet[count.index]
-  tags = merge(var.tags , {Name = var.env})
+  tags = merge(var.tags , {Name = "public_subnet"})
 }
